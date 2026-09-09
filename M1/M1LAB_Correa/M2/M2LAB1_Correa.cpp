b6f85babaee624b0dedc85977450bd6785a4bad2
RicoTorresCuts <correatj0850@student.faytechcc.edu>
2026-09-09 15:02:04 +0000

    M2LAB1_Correa finished

---
+++ M1/M1LAB_Correa/M2/M2LAB1_Correa.cpp
@@ -0,0 +1,57 @@
+// CSC 134
+// M2LAB1 Product Sales
+// Correa-Torres
+// 09/09/26
+// Program made with Input
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    // Set up variables
+    string product_name;
+    int    product_count;
+    double product_price; // $ USD, per item
+    // variables for customer side
+    int    purchase_count;
+    double purchase_total;
+
+    // GET INPUT -- set up the store
+    cout << "STORE SETUP" <<endl;
+    cout << "Product Name: ";
+    cin >> product_name;
+    cout << "Item Count: ";
+    cin >> product_count;
+    cout << "Price Each: $";
+    cin >> product_price;
+
+    // GET INPUT -- Greet user, get their order
+    cout << "--------------------------------" <<endl <<endl;
+    cout << "WELCOME, CUSTOMER." << endl;
+    cout << "Welcome to the " << product_name << " shot." << endl;
+    cout << "Our " << product_name << "(s) are $" << product_price << " each." << endl;
+
+    cout << "How many would you like today? ";
+    cin >> purchase_count;
+
+    // DO THE PROCESSING
+    // NOTE: we don't verify the user input -- they can buy more than we have, or a negative amount.
+    // This will be fixed in the Loops module.
+    purchase_total = purchase_count * product_price;
+
+    // PRINT OUTPUT
+    cout << "You have ordered " << purchase_count << " " << product_name << "(s)." << endl;
+    cout << "Total price: $" << purchase_total << endl; 
+    cout << "Thank you for shopping with CSC 134." << endl << endl;
+
+
+
+    return 0; // no errors
+
+  
+
+
+
+
+}
