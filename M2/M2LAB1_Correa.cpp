c225443ab0162879a2b46865cb7a18bfe3980a83
RicoTorresCuts <correatj0850@student.faytechcc.edu>
2026-09-16 14:29:09 +0000

    M2t1 and M2Lab1

---
+++ M2/M2LAB1_Correa.cpp
@@ -0,0 +1,23 @@
+// CSC 134
+// M2T1 Product Sales
+// Correa-Torres
+// 09/09/26
+// Program made with Input
+
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    // Set up variables
+
+
+    return 0; // no errors
+
+  
+
+
+
+
+}
